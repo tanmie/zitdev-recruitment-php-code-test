70f0c20bfc8125d09360c11f02a290a56bc67416
tomcen <tanmie06@163.com>
2022-10-07 00:11:57 +0800

    测试两个类型的log单元

--- tests/Service/AppLoggerTest.php
+++ tests/Service/AppLoggerTest.php
@@ -10,10 +10,22 @@ use App\Service\AppLogger;
  */
 class AppLoggerTest extends TestCase
 {
-
+    /**
+     * log4php 单元测试
+     */
     public function testInfoLog()
     {
         $logger = new AppLogger('log4php');
         $logger->info('This is info log message');
+        $this->assertFileExists('./logs/log4php/'.date('Ymd'). '.log');
+    }
+
+    /**
+     * Think-Log 单元测试
+     */
+    public function testThinkLog(){
+        $logger = new AppLogger('think-log');
+        $logger->info('This is think log message');
+        $this->assertFileExists('./logs/think/'.date('Ym') . DIRECTORY_SEPARATOR . date('d'). '_cli.log');
     }
 }
