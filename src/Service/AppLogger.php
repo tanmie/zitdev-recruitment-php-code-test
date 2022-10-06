92e25642c48af9fc7eca54f5ef5ba8845d2309b8
tomcen <tanmie06@163.com>
2022-10-07 00:11:19 +0800

    1-配置不同log日志工具参数 2-判断不同log类型需求调用不同log工具 3-对think-log类型的日志全部转换大写。

--- src/Service/AppLogger.php
+++ src/Service/AppLogger.php
@@ -1,32 +1,89 @@
 <?php
 
 namespace App\Service;
+use think\facade\Log;
 
 class AppLogger
 {
     const TYPE_LOG4PHP = 'log4php';
 
     private $logger;
+    private $type;
 
     public function __construct($type = self::TYPE_LOG4PHP)
     {
+        $this->type = $type;
         if ($type == self::TYPE_LOG4PHP) {
+            \Logger::configure(
+                array(
+                    'rootLogger' => array(
+                        'appenders' => array(
+                            'myConsoleAppender',
+                        ),
+                        'level' => 'DEBUG'
+                    ),
+                    'appenders' =>[
+                        'myConsoleAppender' => [
+                            'class' => 'LoggerAppenderDailyFile',
+                            'layout' => [
+                                'class' => 'LoggerLayoutPattern',
+                                'params' => [
+                                    'conversionPattern' => '%date [%logger] %message%newline',
+                                ],
+                            ],
+                            'params' => [
+                                'file' => './logs/log4php/%s.log',
+                                'datePattern' => 'Ymd',
+                            ]
+                        ]
+                    ]
+                )
+            );
             $this->logger = \Logger::getLogger("Log");
+//            $this->logger->assertLog(true,'isok');
+        } else {
+            $this->logger=Log::instance();
+            $this->logger->init([
+                'default' => 'file',
+                'channels' => [
+                    'file' => [
+                        'type' => 'file',
+                        'path' => './logs/think/',
+                    ],
+                ],
+            ]);
         }
     }
 
     public function info($message = '')
     {
+        $message = $this->LogFormat($this->type, $message);
         $this->logger->info($message);
     }
 
     public function debug($message = '')
     {
+        $message = $this->LogFormat($this->type, $message);
         $this->logger->debug($message);
     }
 
     public function error($message = '')
     {
+        $message = $this->LogFormat($this->type, $message);
         $this->logger->error($message);
+    }
+
+    /**
+     * think-log时消息格式转换为大写
+     * @param $type
+     * @param $message
+     * @return string
+     */
+    public function LogFormat($type, $message)
+    {
+        if ($type == self::TYPE_LOG4PHP) {
+            return $message;
+        }
+        return strtoupper($message);
     }
 }
