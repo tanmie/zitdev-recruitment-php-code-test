0cf63360a64f760ad1febdd92a0dee7159d213ef
tomcen <tanmie06@163.com>
2022-10-07 00:05:10 +0800

    数据单元测试

--- tests/App/DemoTest.php
+++ tests/App/DemoTest.php
@@ -14,11 +14,25 @@ use PHPUnit\Framework\TestCase;
 
 class DemoTest extends TestCase {
 
-    public function test_foo() {
+//    public function test_foo() {
+//
+//    }
 
-    }
-
-    public function test_get_user_info() {
-
+    /**
+     * 测试获取用户数据
+     * result 可以是来自某个连接api的返回json
+     */
+    public function test_get_user_info()
+    {
+        $result = '{ "error": 0, "data": { "id": 1, "username": "hello world" }}';
+        $result_arr = json_decode($result, true);
+        $this->assertEquals(true,is_array($result_arr));
+        if (in_array('error', $result_arr) && $result_arr['error'] == 0) {
+            if (in_array('data', $result_arr)) {
+                $this->assertEquals(0,$result_arr['error']);
+                $this->assertEquals(1,$result_arr['data']['id']);
+                $this->assertEquals('hello world',$result_arr['data']['username']);
+            }
+        }
     }
 }
